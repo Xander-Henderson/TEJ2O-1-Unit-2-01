11c7ac8a28ac16d2056a6570a0c7d62e463f4910
Xander-Henderson <xander.henderson@stu.ocsb.ca>
2025-09-10 17:11:16 +0000

    Says "Xander"

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -1,10 +1,10 @@
 """
-Created by: Mr. Coxall
-Created on: Sep 2020
-This module is a Micro:bit MicroPython program
+Created by: Xander
+Created on: Sep 2025
+This module is a Micro:bit MicroPython program that says Xander.
 """
 
 from microbit import *
 
 
-display.scroll("Hello, World!")
+display.scroll("Xander")
